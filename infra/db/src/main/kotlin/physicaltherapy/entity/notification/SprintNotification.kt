38239f69217e757ebc8ec726fd5052c44c17b62d
choi seoin <inseo@uupfi.com>
2023-11-23 21:53:13 +0900

    feat: JPA Entity Field Annoation 추가

--- infra/db/src/main/kotlin/physicaltherapy/entity/notification/SprintNotification.kt
+++ infra/db/src/main/kotlin/physicaltherapy/entity/notification/SprintNotification.kt
@@ -1,8 +1,10 @@
 package physicaltherapy.entity.notification
 
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.EnumType
 import jakarta.persistence.Enumerated
+import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToOne
 import physicaltherapy.entity.BaseEntity
 import physicaltherapy.entity.notificationChannel.NotificationChannel
@@ -10,16 +12,21 @@ import physicaltherapy.entity.notificationChannel.NotificationChannel
 @Entity
 internal class SprintNotification(
 
+    @Column(name = "sprint_id", nullable = false)
     val sprintId: Long,
 
     @ManyToOne
+    @JoinColumn(name = "notification_channel_id", nullable = false)
     val notificationChannel: NotificationChannel,
 
+    @Column(name = "content", nullable = false)
     val content: String,
 
-    val reactionCount: Int,
+    @Column(name = "reaction_count")
+    private var reactionCount: Int = 0,
 
     @Enumerated(EnumType.STRING)
+    @Column(name = "notification_type", nullable = false)
     val type: NotificationType,
 
 ) : BaseEntity()
