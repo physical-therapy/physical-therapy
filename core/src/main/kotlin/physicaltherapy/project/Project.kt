5c82eabec4d7ae51e1f8113f789d12d684ee86db
kyeongrok.kim <kyeongrok.kim@kurlycorp.com>
2024-02-01 20:05:12 +0900

    feat: ProjectStatus 추가

--- core/src/main/kotlin/physicaltherapy/project/Project.kt
+++ core/src/main/kotlin/physicaltherapy/project/Project.kt
@@ -9,10 +9,12 @@ data class Project(
     val recruitmentEndDate: LocalDateTime,
     val master: String,
     val cycle: Int,
+    val projectStatus: ProjectStatus,
     val startDate: LocalDate,
     val endDate: LocalDate,
     val id: Long = 0L,
 ) {
+
     init {
         require(startDate < endDate) { "스타디 시작 시점이 종료 시점보다 늦을 수 없습니다." }
         require(recruitmentEndDate.toLocalDate() < startDate) { "스터디 모집일이 시작일보다 늦을 수 없습니다." }
